e7b398b50644f1a4264e83ac7a4c1cd56bfb16e5
Steve Schmechel <shmakes@gmail.com>
2011-03-29 22:07:44 -0500

    Remove boilerplate.

--- evently/flight_detail/loggedIn/selectors/button.modalInput/click.js
+++ evently/flight_detail/loggedIn/selectors/button.modalInput/click.js
@@ -1,20 +1,3 @@
-/*
-   Copyright 2011 Steve Schmechel
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License
-*/
-
-
 function() {
   var vetId = $(this).parent().parent().attr("vetid");
   $("#vet_name")[0].textContent = $(this).parent().siblings()[1].textContent;
@@ -29,7 +12,6 @@ function() {
       var zipSel = $("select[name='ByZipSel']");
       zipSel.find('option').remove().end();
       var zipOpt = zipSel.attr('options');
-      // TODO: get left 5 digits as int and increment for endKey.
       app.db.view("basic/guardians_by_zip", {
         descending : false,
         limit: 10,
