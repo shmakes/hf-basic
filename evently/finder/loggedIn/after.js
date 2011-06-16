87f85aa944b65d9d6f11e6c3ebb8b2054b722ec3
Steve Schmechel <shmakes@gmail.com>
2011-06-15 21:37:18 -0500

    Update finder filters immediately.

--- evently/finder/loggedIn/after.js
+++ evently/finder/loggedIn/after.js
@@ -1,4 +1,10 @@
 function() {
+  $('#status').change(function() {
+    $('#last_name').trigger('keyup');
+  });
+  $('#flight').change(function() {
+    $('#last_name').trigger('keyup');
+  });
   $(this).show();
   $("#last_name").focus();
 };
