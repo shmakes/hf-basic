cfc7604aae047b4d30fbb6a9e64e504e38f3d0a6
Steve Schmechel <shmakes@gmail.com>
2011-06-30 21:53:47 -0500

    Test fill by zip code data feature.

---
+++ evently/grdedit/loggedIn/selectors/#zipFill/click.js
@@ -0,0 +1,17 @@
+function() {
+  var zip = $("input[name='zip']").val();
+  if (zip.length == 5) {
+    var app = $$(this).app;
+
+    app.db.openDoc("loc-" + zip, {
+      success : function(doc) {
+        $("input[name='city']").val(doc.city)
+        $("input[name='state']").val(doc.state)
+        $("input[name='county']").val(doc.county)
+      }
+    });
+  }
+  return false;
+};
+
+//@ sourceURL=grdedit/loggedIn/selectors/#zipFill~click.js
