31a9bbbca5b2813a0552136aac4a609e60af44f4
Steve Schmechel <shmakes@gmail.com>
2011-07-12 22:03:09 -0500

    Add type to recent addition and changes views.

--- views/admin_recent_additions/map.js
+++ views/admin_recent_additions/map.js
@@ -1,6 +1,6 @@
 function(doc) {
   if (doc.name.last) {
     emit(doc.metadata.created_at, 
-    [doc.name.first + ' ' + doc.name.last, doc.metadata.created_by]);
+    [doc.type, doc.name.first + ' ' + doc.name.last, doc.metadata.created_by]);
   }
 }
--- views/admin_recent_changes/map.js
+++ views/admin_recent_changes/map.js
@@ -1,6 +1,6 @@
 function(doc) {
   if (doc.name.last) {
     emit(doc.metadata.updated_at, 
-    [doc.name.first + ' ' + doc.name.last, doc.metadata.updated_by]);
+    [doc.type, doc.name.first + ' ' + doc.name.last, doc.metadata.updated_by]);
   }
 }
