71c26f4816674f412a878d50eb264abd511b6b98
Steve Schmechel <shmakes@gmail.com>
2011-07-14 22:19:30 -0500

    Adding batches of vets now works.

--- evently/flight_detail/loggedIn/selectors/#addVets/click.js
+++ evently/flight_detail/loggedIn/selectors/#addVets/click.js
@@ -5,42 +5,42 @@ function() {
   var vetQty = $("#vetQty").val();
   var timestamp = ISODateString(new Date());
 
-  if (vetQty < 1) {
+  if ((vetQty < 1) || (vetQty > 100)) {
     return true;
   }
 
-  while (vetQty > 0) {
+  var added = 0;
 
-    app.db.view("basic/waitlist_veterans", {
-      limit : 1,
-      descending : false,
-      include_docs: true,
-      type : "newRows",
-      success: function(resp) {
-        if (resp.rows.length > 0) {
-          doc = resp.rows[0].doc;
-          oldFlight = doc.flight.id;
-          doc.flight.id = flightName;
-          doc.flight.history.push({
-            id: timestamp,
-            change: "changed flight from: " + oldFlight + " to: " + flightName + " by: " + user
-          });
+  app.db.view("basic/waitlist_veterans", {
+    limit : vetQty,
+    descending : false,
+    include_docs: true,
+    type : "newRows",
+    success: function(resp) {
+      rowCount = resp.rows.length;
+      for (row in resp.rows) {
+        doc = resp.rows[row].doc;
+        oldFlight = doc.flight.id;
+        doc.flight.id = flightName;
+        doc.flight.history.push({
+          id: timestamp,
+          change: "changed flight from: " + oldFlight + " to: " + flightName + " by: " + user
+        });
 
-          app.db.saveDoc(doc, {
-            success : function() {
+        app.db.saveDoc(doc, {
+          success : function() {
+            added++;
+            if (added == rowCount) {
+              //alert("Added " + added.toString());
+              window.location.reload();
             }
-          });
-        }
+          }
+        });
       }
-    });
-
-    vetQty--;
-  }
-
-
+    }
+  });
 
   $("#vetQty").val("0");
-
   return true;
 };
 
