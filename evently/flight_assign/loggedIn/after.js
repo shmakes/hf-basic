c96fefb8265798c838ca873003520e439c3d2bf9
Steve Schmechel <shmakes@gmail.com>
2011-07-21 22:45:42 -0500

    Added counts to top of flight assignment form.
    Closes #30

--- evently/flight_assign/loggedIn/after.js
+++ evently/flight_assign/loggedIn/after.js
@@ -1,5 +1,15 @@
 function() {
 
+  numVets = $("tr.Veteran").length;
+  $("#vetCount").val(numVets.toString());
+  
+  numGrds = $("tr.Guardian").length;
+  $("#grdCount").val(numGrds.toString());
+
+  capacity = parseInt($("#flightCap").val());
+  remaining = capacity - numVets - numGrds;
+  $("#remainCount").val(remaining.toString());
+  
   $(this).show();
 };
 
