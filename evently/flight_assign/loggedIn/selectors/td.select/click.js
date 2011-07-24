ad55461a51f50e4a137add871ad2d9269902f3ff
Steve Schmechel <shmakes@gmail.com>
2011-07-24 01:25:04 -0500

    Simplified selection of people on flight detail.

---
+++ evently/flight_assign/loggedIn/selectors/td.select/click.js
@@ -0,0 +1,15 @@
+function() {
+  var row = $(this).parent();
+  var docId = row.attr("id");
+  var docType = row.attr("class");
+
+  if (docType == "Veteran") {
+    window.open("vet_edit.html?vetid=" + docId, '_blank');
+  }
+  if (docType == "Guardian") {
+    window.open("grd_edit.html?grdid=" + docId, '_blank');
+  }
+
+};
+
+//@ sourceURL=flight_detail/loggedIn/selectors/td#person~click.js
--- evently/flight_assign/loggedIn/selectors/td[name='Veteran']/click.js
+++
@@ -1,6 +0,0 @@
-function() {
-  var vetId = $(this).parent().attr("id");
-  window.open("vet_edit.html?vetid=" + vetId, '_blank');
-};
-
-//@ sourceURL=flight_detail/loggedIn/selectors/td#vet_name~click.js
