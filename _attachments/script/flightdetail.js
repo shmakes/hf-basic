e1543ee26e59e58988c2f8cb3ffa074fd5865b9c
Steve Schmechel <shmakes@gmail.com>
2011-07-23 23:48:59 -0500

    Fixed grid update after pairing.

--- _attachments/script/flightdetail.js
+++ _attachments/script/flightdetail.js
@@ -7,12 +7,19 @@ function UpdateGuardianDataGrid(vetId, grd) {
   // Update the grid.
   var vetRow = $("tr.DataRow[vetid='" + vetId + "']");
   var vetData = vetRow.children("td");
-  vetData[13].textContent = (grd.name ? (grd.name.first + " " + grd.name.last) : "");
-  vetData[12].textContent = (grd.address ? (grd.address.city + ", " + grd.address.state) : "");
-  vetData[11].textContent = (grd.address ? grd.app_date : "");
-  vetData[10].textContent = (grd.veteran ? grd.veteran.pref_notes : "");
-  vetData[9].textContent  = (grd.flight ? grd.flight.bus : "");
-  vetData[8].children("input").val(grd.flight.seat);
+  if (grd.name) {
+    vetData[10].textContent = grd.name.first + " " + grd.name.last;
+    vetData[9].textContent = grd.address.city + ", " + grd.address.state;
+    vetData[8].textContent = grd.medical.experience;
+    vetData[7].textContent  = grd.flight.bus;
+    vetData[6].children("input").val(grd.flight.seat);
+  } else {
+    vetData[10].textContent = "";
+    vetData[9].textContent = "";
+    vetData[8].textContent = "";
+    vetData[7].textContent  = "";
+    vetData[6].children("input").val("");
+  }
 }
 
 function PairGuardianToVeteran(app, vetId, grdIdNew, user) {
