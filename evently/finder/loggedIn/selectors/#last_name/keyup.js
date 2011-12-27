a83b722fd66d2828a8756df1cc294bcec307a4e8
Steve Schmechel <shmakes@gmail.com>
2011-12-27 15:23:43 -0600

    Marker for end of direct matches.

--- evently/finder/loggedIn/selectors/#last_name/keyup.js
+++ evently/finder/loggedIn/selectors/#last_name/keyup.js
@@ -45,11 +45,20 @@ function() {
     descending : false,
     type : "newRows",
     success: function(resp) {
-      var id, person, anchor, tr;
+      var id, person, anchor, tr, lastName, endMarker;
+      endMarker = true;
       for (row in resp.rows) {
         anchor = "<a href='#'>";
         id = resp.rows[row].id;
         person = resp.rows[row].value;
+        lastName = resp.rows[row].key.pop().toLowerCase();
+
+        if ((endMarker) && (lastName > (value + "\ufff0"))) {
+          tr = $("<tr/>", { class: "Separator" });
+          tr.append("<td colspan=6 > ***** End of direct matches ***** </td>");
+          rslt.append(tr);  
+          endMarker = false;
+        }
 
         if (person.type == 'Veteran') {
           anchor = "<a href='vet_edit.html?vetid=" + id + "' target='_blank'>";
@@ -66,7 +75,8 @@ function() {
         tr.append("<td>" + anchor + person.flight + "</a></td>");
         tr.append("<td>" + anchor + person.status + "</a></td>");
         tr.append("<td>" + anchor + person.pairing + "</a></td>");
-        rslt.append(tr);     
+        rslt.append(tr);  
+
       }
     }
   })
