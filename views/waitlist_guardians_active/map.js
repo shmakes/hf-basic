7f06e52f23de1f2d546eac70bd947b38caffcf11
Steve Schmechel <shmakes@gmail.com>
2015-09-22 19:26:38 -0500

    Fix flight loading so it ignores "future" on wait list even though they show up now.
    Closes #133

---
+++ views/waitlist_guardians_active/map.js
@@ -0,0 +1,11 @@
+function(doc) {
+  var statuses = ["Active", "Future-Fall", "Future-Spring"];
+  
+  if ((doc.type == "Guardian") 
+      && (doc.flight.status == "Active")
+      && (statuses.indexOf(doc.flight.status) != -1)
+      && (doc.flight.id == "None")
+      && (doc.veteran.pairings.length === 0)) {
+    emit(doc.app_date, null);
+  }
+}
---
+++ views/waitlist_veterans_active/map.js
@@ -0,0 +1,10 @@
+function(doc) {
+  var statuses = ["Active", "Future-Fall", "Future-Spring"];
+  var conflict = ["WWII", "Korea", "Vietnam", "Afghanistan", "Iraq"];
+  if ((doc.type == "Veteran") 
+      && (doc.flight.status == "Active")
+      && (statuses.indexOf(doc.flight.status) != -1)
+      && (doc.flight.id == "None")) {
+    emit(conflict.indexOf((doc.vet_type || 'WWII')) + "-" + doc.app_date, doc.flight.group);
+  }
+}
